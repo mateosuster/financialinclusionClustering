57c67d9a4a304919b5c4dc1c008a3a3356fb23ae
mateosuster <mateosuster@gmail.com>
2022-06-23 13:48:21 -0300

    agrego data parcial mpe3

--- code/get_data.py
+++ code/get_data.py
@@ -3,7 +3,8 @@ import wbdata
 import pandas as pd
 import os
 
-os.chdir("C:/Users/mateo/Documents/repos/financialinclusionClustering")
+# os.chdir("C:/Users/mateo/Documents/repos/financialinclusionClustering")
+os.chdir('/home/mateo1/repos/financialinclusionClustering')
 
 # rcParams['figure.figsize'] = 40, 12
 
@@ -77,3 +78,22 @@ data = filter_data.drop("Indicator Name", axis=1).pivot(index = ["Country Name",
 data.rename(columns = var_dict,inplace=True)
 
 data.to_csv("data/data.csv") #exportacion de dataset
+
+
+data_parcial = pre_data[(pre_data['Indicator Code'].isin(['fin1.t.a.1',
+                                                          'fin1.t.a.2',
+                                                          'fin18.t.d.1',
+                                                          'fin18.t.d.2'])) 
+                        &(pre_data["Country Code"].isin(countries.loc[countries["Region"].notnull() , "Country Code"].values))]
+
+data_parcial= pd.merge(data_parcial, countries[["Country Code", "Region"]] , how ="left", on = "Country Code")
+
+x = data_parcial.pivot(index = ['Region', 'Country Name', 'Country Code', 'year'] , 
+                   columns = 'Indicator Name', 
+                   values = 'value')
+
+x.to_csv('/home/mateo1/docencia/data_parcial_2.csv')
+
+data_parcial.info()
+
+len(pd.unique(data_parcial['Country Name']))
