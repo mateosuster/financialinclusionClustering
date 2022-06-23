c2f2f54c7f779b22bb8479197afeafc7ba395d0b
mateosuster <mateosuster@gmail.com>
2022-06-23 10:20:03 -0300

    agrego distribuciones

--- code/clustering_rf.R
+++ code/clustering_rf.R
@@ -1,43 +1,110 @@
 require("data.table")
 require("randomForest")
-
+library(tidyverse)
+library(cluster)    # clustering algorithms
+library(factoextra) # clustering visualization  
 
 #limpio la memoria
 rm( list=ls() )  #remove all objects
 gc()             #garbage collection
 
-setwd('C:/Users/mateo/Documents/repos/financialinclusionClustering')
+setwd('/home/mateo1/repos/financialinclusionClustering')
 
 #leo el dataset , aqui se puede usar algun super dataset con Feature Engineering
-dataset  <- fread( "data/data.csv")
+dataset <- read.csv('results/data_imputada_2.csv') 
 glimpse(dataset)
 
+data = dataset[, 3:ncol(dataset) ]
+index = dataset[, 1:3]
 
 #quito los nulos para que se pueda ejecutar randomForest,  Dios que algoritmo prehistorico ...
 # dataset  <- na.roughfix( dataset )
-dataset  <- na.omit(dataset) 
+# dataset  <- na.omit(dataset) 
 gc()
 
+rf_list = list()
+dist_list = list()
+hclust_list = list()
+sil_list = list()
 
-#Ahora, a esperar mucho con este algoritmo del pasado que NO correr en paralelo, patetico
-modelo  <- randomForest( x= dataset[, 3:ncol(dataset) ] , 
-                         y= NULL, 
-                         ntree= 100, #se puede aumentar a 10000
-                         proximity= TRUE, 
-                         oob.prox = TRUE )
+df_sil <- data.frame(matrix(ncol = 3, nrow = 0))
+colnames(df_sil) <- c("imputer", "ncluster", "avg_sil")
 
-#genero los clusters jerarquicos
-hclust.rf  <- hclust( as.dist ( 1.0 - modelo$proximity),  #distancia = 1.0 - proximidad
-                      method= "ward.D2" )
+for(imputer_i in unique(data$imputer)){
+  print(imputer_i)
+  
+  modelo  <- randomForest( x= data %>%
+                             filter(imputer== imputer_i)  %>%
+                             select(-imputer)
+                           ,
+                           y= NULL,
+                           ntree= 100, #se puede aumentar a 10000
+                           proximity= TRUE,
+                           oob.prox = TRUE )
+
+  
+  #genero los clusters jerarquicos
+  dist_mtrx <-  as.dist ( 1.0 - modelo$proximity) #distancia = 1.0 - proximidad
+  
+  hclust.rf  <- hclust( dist_mtrx,
+                        method= "ward.D2" )
+  
+  for (nclust in c(2:15)){
+    print(nclust)
+    rf.cluster  <- cutree( hclust.rf, nclust) #corto los arboles
+    sil <- silhouette (rf.cluster,dist_mtrx) # or use your cluster vector
+    sil_avg <- summary(sil)[[4]]
+    
+    row <- c("imputer" = imputer_i, "ncluster"=nclust, "avg_sil" = sil_avg) 
+    df_sil <-  rbind(df_sil, data.frame(t(row)) )
+  }
+    
+  
+  rf_list[[imputer_i]] = modelo
+  dist_list[[imputer_i]] = dist_mtrx
+  hclust_list[[imputer_i]] = hclust.rf
+  # sil_list[[imputer_i]] = df_sil
+  
+  # df_sil <- data.frame(matrix(ncol = 3, nrow = 0))
+  # colnames(df_sil) <- c("imputer", "ncluster", "avg_sil")
+  
+}
+
+#grafico silhoutte
+df_sil %>% 
+  mutate_at(.vars = c("ncluster", "avg_sil"), .funs = as.double) %>% 
+  ggplot(aes(ncluster, avg_sil, color = imputer)) +
+  geom_line()+ geom_point()+
+  theme(legend.position = 'bottom')+
+  labs(y = 'Silhouette promedio', x = 'Cantidad de clústers')
+ggsave(filename = 'results/sil_avg.jpg')
 
 
-pdf( paste0( paste0("results/cluster_jerarquico.pdf" ) ))
-plot( hclust.rf )
-dev.off()
+# cluster plot for max sil
+for (dist_list , hclust_list ){
+  print(nclust)
+  rf.cluster  <- cutree( hclust.rf, nclust) #corto los arboles
+  sil <- silhouette (rf.cluster,dist_mtrx) # or use your cluster vector
+  sil_avg <- summary(sil)[[4]]
+  
+  row <- c("imputer" = imputer_i, "ncluster"=nclust, "avg_sil" = sil_avg) 
+  df_sil <-  rbind(df_sil, data.frame(t(row)) )
+}
+
+  
 
 
+
+
+
+
+
+# DMeyf
 h <- 20
 distintos <- 0
+dataset <- as.data.table(data %>% 
+                           filter(imputer== "Complete")  %>% 
+                           select(-imputer))
 
 while(  h>0  &  !( distintos >=6 & distintos <=7 ) )
 {
--- code/plots.R
+++ code/plots.R
@@ -8,7 +8,7 @@ library(VIM)
 library(mice) # Cargamos la librería
 
 #wd
-setwd('C:/Users/mateo/Documents/repos/financialinclusionClustering')
+setwd('/home/mateo1/repos/financialinclusionClustering')
 
 #data
 data = read.csv("data/data.csv")
@@ -21,6 +21,34 @@ summary(aggr(data, sortVar=TRUE, plot=F))
 # aggr(data, sortVar=TRUE, oma = c(16, 5, 5, 3), numbers=T)
 aggr(data, sortVar=TRUE)
 
+# count missings
+count_missing = data %>%
+  arrange(Country.Name) %>% 
+  group_by(Country.Name) %>%
+  summarise_each(funs(sum(is.na(.)))) %>% 
+  # summarise_at(vars(-Country.Name), sum)
+  select(-Country.Name) %>%
+  # group_by(Country.Name)  %>% 
+  rowSums() %>% 
+  cbind(unique(data$Country.Name)) %>% 
+  as_data_frame() %>% 
+  rename("missing_count" = ".", "country" = "V2") %>%  
+  mutate(missing_count=as.double(missing_count))
+
+count_missing %>% 
+  mutate(missing_count=as.double(missing_count)) %>% 
+  # filter(missing_count>10) %>% 
+  filter(missing_count>quantile(count_missing$missing_count, probs = 0.75)) %>% 
+  ggplot(aes(x = fct_rev(fct_reorder(country, missing_count)), y = missing_count)) +
+  geom_col()+
+  theme(axis.text.x = element_text(angle = 45 
+                                   ,vjust = 0.5 
+                                   # ,hjust=1
+                                   ))+
+  labs(x = "Países", y = "Missings")
+ggsave(filename = "results/missings_x_pais.jpg")
+
+#patron de missings
 na_pattern_plot= md.pattern(data, rotate.names=T, plot = T)
 ggsave('results/na_patter_plot.jpg', na_pattern_plot)
 
@@ -29,7 +57,7 @@ data.frame(na_pattern_plot)
 
 #plot
 ggpairs(data=data, columns = 4:ncol(data) )
-ggsave("ggpair.jpg")
+ggsave("results/ggpair.jpg")
 
 
 # imputacion hot deck
@@ -41,7 +69,9 @@ hotdeck_imp$imputer = 'HotDeck'
 data_imp = read.csv('results/data_imputada.csv') %>% 
   bind_rows(hotdeck_imp) %>% 
   bind_rows(data %>% 
-              mutate(imputer= 'Original'))
+              na.omit() %>% 
+              mutate(imputer= 'Complete'))
+write.csv(data_imp, file = "results/data_imputada_2.csv")
 
 table(data_imp$imputer)
  
@@ -60,6 +90,7 @@ for (i in names(data_imp[, 4:ncol(data_imp) ])) {
                 quantiles = 4,
                 quantile_lines = TRUE,   vline_size = 0.5, vline_color = "red",
                 alpha = .5
+                , jittered_points = TRUE, point_alpha = 0.3
               ) + 
               # scale_point_color_hue(l = 40) +
               # scale_discrete_manual(aesthetics = "point_shape", values = c(21, 22, 23, 24, 25))+
